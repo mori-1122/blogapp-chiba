063f2af0b92def1ad861a00204a057e1044f8acf
chiba <edyzrg1118@gmail.com>
2025-03-16 04:05:33 +0900

    フォローの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,10 @@ class User < ApplicationRecord
 has_many :articles, dependent: :destroy
 has_many :likes, dependent: :destroy
 has_many :favorite_articles, through: :likes, source: :article
+
+has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+has_many :followings, through: :following_relationships, source: :following
+
 has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -46,6 +50,10 @@ has_one :profile, dependent: :destroy
 
 # ぼっち演算子
   profile&.nickname || self.email.split('@').first
+end
+
+def follow!(user)
+  following_relationships.create!(following_id: user.id)
 end
 
   # def birthday
