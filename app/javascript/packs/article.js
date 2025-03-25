d2639869994d8fd7873387a8383f72dcc3c0dbf7
chiba <edyzrg1118@gmail.com>
2025-03-26 04:46:39 +0900

    ハートの処理を別ファイルに

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,6 +1,10 @@
 import $ from 'jquery'
 import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
+import {
+    listenInactiveHeartEvent,
+    listenActiveHeartEvent
+} from 'modules/handle_heart'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
@@ -36,6 +40,10 @@ document.addEventListener('DOMContentLoaded', () => {
             appendNewComment(comment)
         })
       })
+      .catch((e) => {
+        window.alert('失敗しました')
+        console.log(e)
+    })
 
     handleCommentForm()
 
@@ -60,31 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const hasLiked = response.data.hasLiked
             handleHeartDisplay(hasLiked)
         })
-    $('.inactive-heart').on('click', () => {
-        axios.post(`/articles/${articleId}/like`)
-        .then((response) => {
-            if (response.data.status === 'ok') {
-                $('.active-heart').removeClass('hidden')
-                $('.inactive-heart').addClass('hidden')
-            }
-        })
-        .catch((e) => {
-            window.alert('失敗しました')
-            console.log(e)
-        })
-    })
 
-    $('.active-heart').on('click', () => {
-        axios.delete(`/articles/${articleId}/like`)
-        .then((response) => {
-           if (response.data.status === 'ok') {
-            $('.active-heart').addClass('hidden')
-            $('.inactive-heart').removeClass('hidden')
-           }
-        })
-        .catch((e) => {
-            window.alert('失敗しました')
-            console.log(e)
-        })
-    })
+listenInactiveHeartEvent(articleId)
+  listenActiveHeartEvent(articleId)
 })
