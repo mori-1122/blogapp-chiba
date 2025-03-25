4babe59a2343ced08d27a6aaaa1119aa4eacd50b
chiba <edyzrg1118@gmail.com>
2025-03-25 10:02:28 +0900

    ajaxでいいねの状態

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,15 +10,19 @@ class LikesController < ApplicationController
   def create
     article = Article.find(params[:article_id])
     article.likes.create!(user_id: current_user.id)
-    redirect_to article_path(article)
+
+    render json: { status: 'ok'}
   end
 
   def destroy
-     article = Article.find(params[:article_id])
-     like = article.likes.find_by!(user_id: current_user.id)
-     like.destroy!
-     redirect_to article_path(article)
+    article = Article.find(params[:article_id])
+    like = article.likes.find_by!(user_id: current_user.id)
+    like.destroy!
+
+    render json: { status: 'ok'}
   end
 end
 
 
+
+# 3-6-6 いいね状態をaxiosで変更
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -21,8 +21,10 @@ require("@rails/actiontext")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
 
-document.addEventListener('turbolinks:load', () => {
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
 const handleHeartDisplay = (hasLiked) => {
     if (hasLiked) {
         $('.active-heart').removeClass('hidden')
@@ -30,12 +32,34 @@ const handleHeartDisplay = (hasLiked) => {
         $('.inactive-heart').removeClass('hidden')
       }
 }
-
+document.addEventListener('DOMContentLoaded', () => {
     const dataset = $('#article-show').data()
     const articleId = dataset.articleId
-  axios.get(`/articles/${articleId}/like`)
-  .then((response) => {
+    axios.get(`/articles/${articleId}/like`)
+          .then((response) => {
             const hasLiked = response.data.hasLiked
             handleHeartDisplay(hasLiked)
         })
+    $('.inactive-heart').on('click', () => {
+        axios.post(`/articles/${articleId}/like`)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((e) => {
+            window.alert('失敗しました')
+            console.log(e)
+        })
+    })
+
+    $('.active-heart').on('click', () => {
+        axios.delete(`/articles/${articleId}/like`)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((e) => {
+            window.alert('失敗しました')
+            console.log(e)
+        })
+    })
 })
+
