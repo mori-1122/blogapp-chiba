f03d2a5e12b8b465d2c78e3d6d09c9e1f6e32646
chiba <edyzrg1118@gmail.com>
2025-03-25 10:22:06 +0900

    いいねの状態をjQueryで反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -43,7 +43,10 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.inactive-heart').on('click', () => {
         axios.post(`/articles/${articleId}/like`)
         .then((response) => {
-            console.log(response)
+            if (response.data.status === 'ok') {
+                $('.active-heart').removeClass('hidden')
+                $('.inactive-heart').addClass('hidden')
+            }
         })
         .catch((e) => {
             window.alert('失敗しました')
@@ -54,7 +57,10 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.active-heart').on('click', () => {
         axios.delete(`/articles/${articleId}/like`)
         .then((response) => {
-            console.log(response)
+           if (response.data.status === 'ok') {
+            $('.active-heart').addClass('hidden')
+            $('.inactive-heart').removeClass('hidden')
+           }
         })
         .catch((e) => {
             window.alert('失敗しました')
